dc1f9abe8a42b8b0dde46517972ace2865e1560a
Alex Mengue <alexmengue1990@hotmail.com>
2021-02-26 17:31:31 -0300

    Quarta aula

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
+
 import styles from '../../styles/components/Profile.module.css';
 
 function Profile() {
+  const {level} = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/alexmengue.png" alt="Alex mengue" />
@@ -8,7 +13,7 @@ function Profile() {
         <strong>Alex Mengue</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
